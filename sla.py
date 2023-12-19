fc3c556ad71b1b4c7a8e344d17251fe8d48b275e
limaeloisa <144168277+limaeloisa@users.noreply.github.com>
2023-12-19 16:58:16 -0300

    Add files via upload

---
+++ sla.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+e = Turtle()
+
+def circle():
+    e.circle(34)
+
+def squad():
+    for i in range():
+        fd(100)
+        lt(90)
+
+escolha = " "
+
+while(escolha !=0):
+    print("---------------------------")
+    print(" qual forma vc quer ver sendo feita?")
+    print("1: Circle")
+    print("2: squad")
+    if escolha == 1:
+        circle()
+    if escolha == 2:
+        squad()
+
+
+mainloop()
